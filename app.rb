e9ac230939afad6d84add6c42120a5be7076ecf7
Roxanne Gutierrez <roxannenguyenn@gmail.com>
2023-04-15 15:25:08 -0500

    commit tree d6d83c205c24da22fcc476295a55306492c0fd87

--- app.rb
+++ app.rb
@@ -1,23 +1,10 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+puts "Hello World!"
 
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
+print "Pass this test, please."
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+p [1,2,3]
 
-puts add(1,3) + add(2,2)
+
+
+
